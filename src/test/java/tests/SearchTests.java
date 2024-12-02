8a4c75a42c40761cbba45bfc0d00a13c773e3fcc
Pavel Bakanov <88797455+PavelBakanov@users.noreply.github.com>
2024-12-02 16:21:52 +0300

    Update SearchTests.java

--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -3,11 +3,10 @@ package tests;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
-import static io.appium.java_client.AppiumBy.accessibilityId;
-import static io.appium.java_client.AppiumBy.id;
+import static com.codeborne.selenide.Selenide.*;
+import static io.appium.java_client.AppiumBy.*;
 import static io.qameta.allure.Allure.step;
+
 
 public class SearchTests extends TestBase {
     @Test
