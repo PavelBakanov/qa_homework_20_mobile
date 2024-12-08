d36504d46108b0bd2d44042cec6182bd017f8c71
PavelBakanov <maxconghunter@mail.ru>
2024-12-08 21:22:48 +0300

    minor changes

--- src/test/java/config/EmulationConfig.java
+++ src/test/java/config/EmulationConfig.java
@@ -3,7 +3,7 @@ package config;
 import org.aeonbits.owner.Config;
 
 @Config.Sources({
-        "classpath:properties/${deviceHost}.properties",
+        "classpath:properties/${deviceHost}.properties"
 })
 public interface EmulationConfig extends Config {
 
